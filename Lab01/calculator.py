9f499fd7828a9049443cf08f95b885e7d7d3be90
484805 <matricola484805@gmail.com>
2020-09-18 11:03:45 +0200

    Lab01: UnitTests

--- Lab01/calculator.py
+++ Lab01/calculator.py
@@ -1,6 +1,3 @@
-# Note: following strictly the instructions,
-#       so it won't work for negative integers
-
 def sum(m, n):
     while n > 0:
         n -= 1
@@ -11,6 +8,8 @@ def sum(m, n):
     return m
 
 def divide(m, n):
+    if n == 0:
+        raise ZeroDivisionError
     k = 0
     s = -1 if (m < 0) != (n < 0) else 1
     m, n = abs(m), abs(n)
